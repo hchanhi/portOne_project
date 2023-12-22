090e93bf087e0f94b07343f913dc2d254820ac1f
Hchan <hchani95@gamil.com>
2023-12-22 15:36:51 +0900

    test : 프로젝트 기본 설정
    스프링 부트로 프로젝트 기본 설정

---
+++ src/main/java/com/portone/payment/PortOneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.portone.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortOneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortOneProjectApplication.class, args);
+    }
+
+}
