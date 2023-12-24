06c1a5ac09d6d6ba756ac50efd63eb342daf9e43
Hchan <hchani95@gamil.com>
2023-12-24 23:39:20 +0900

    fix : 포트원 sdk 호출 파라미터 수정

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -13,21 +13,21 @@ function requestPay() {
                 merchant_uid: merchant_uid,
                 name: productName,
                 amount: paymentAmount,
-                buyer_email: "Iamport@chai.finance",
-                buyer_name: "포트원 기술지원팀",
+                buyer_email: "hcahni@aaa.com",
+                buyer_name: "한**",
                 buyer_tel: "010-1234-5678",
-                buyer_addr: "서울특별시 강남구 삼성동",
+                buyer_addr: "서울특별시 ***구 **동",
                 buyer_postcode: "123-456",
             },
             function (rsp) {
                 if (rsp.success) {
                     // 결제 성공 시: 결제 승인 또는 가상계좌 발급에 성공한 경우
-                    alert("결제가 성공적으로 처리되었습니다.\n 거래 번호 : " + rsp.imp_uid);
                     console.log(rsp.imp_uid);
+                    alert("결제가 성공적으로 처리되었습니다.\n 거래 번호 : " + rsp.imp_uid);
 
                 } else {
                     alert("결제에 실패하였습니다. 에러 내용: " + rsp.error_msg +
-                    "거래 번호: " + orderNumber);
+                    "거래 번호: " + merchant_uid);
                 }
             });
     }
