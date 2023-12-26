012297efbbd92d2d1acf48c34a8baf6cd08c2367
Hchan <hchani95@gamil.com>
2023-12-26 16:34:20 +0900

    feat : 거래 단건 조회 후 주문 취소 서비스 추가

--- src/main/java/com/portone/payment/controller/PaymentController.java
+++ src/main/java/com/portone/payment/controller/PaymentController.java
@@ -1,6 +1,9 @@
 package com.portone.payment.controller;
 
 import com.portone.payment.service.PaymentService;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,8 +35,27 @@ public class PaymentController {
         String imp_uid = payload.get("imp_uid");
 
         ResponseEntity<String> response = paymentService.checkPayment(imp_uid);
+        ResponseEntity<String> cancelResponse = null;
 
         if (response.getStatusCode().is2xxSuccessful()) {
+                JSONObject Json = null;
+                JSONObject JsonResponse = null;
+                String status = null;
+                try {
+                    JSONParser parser = new JSONParser();
+                    Json = (JSONObject) parser.parse(String.valueOf(response.getBody()));
+                    JsonResponse = (JSONObject) Json.get("response");
+                    status = (String) JsonResponse.get("status");
+                    System.out.println("payment status : " + status);
+
+                    if(status.equals("paid")){
+                        System.out.println("cancel payments");
+                        cancelResponse = paymentService.cancelPayment(imp_uid);
+                        return ResponseEntity.ok().body(cancelResponse.getBody());
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
             return ResponseEntity.ok().body(response.getBody());
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response.getBody());
--- src/main/java/com/portone/payment/service/PaymentService.java
+++ src/main/java/com/portone/payment/service/PaymentService.java
@@ -22,25 +22,27 @@ public class PaymentService {
     @Value("${portone.impSecret}")
     private String imp_secret;
     private static String accessToken;
-    private static long expired_at;
+    private static long expired_at = 0;
     private final String portOneUrl = "https://api.iamport.kr";
 
+    RestTemplate restTemplate = new RestTemplate();
+
     public ResponseEntity<Object> getAccessToken() {
-        RestTemplate restTemplate = new RestTemplate();
+        String url = portOneUrl + "/users/getToken";
         Map<String, String> error = new HashMap<>();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("imp_key", imp_key);
-        map.add("imp_secret", imp_secret);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("imp_key", imp_key);
+        body.add("imp_secret", imp_secret);
 
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
 
         ResponseEntity<String> response = null;
             try {
-                response = restTemplate.postForEntity(portOneUrl + "/users/getToken", request, String.class);
+                response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
             } catch (HttpClientErrorException | HttpServerErrorException e) {
                 return ResponseEntity.status(e.getStatusCode()).body(e.getStackTrace());
             }
@@ -72,21 +74,46 @@ public class PaymentService {
 
 
     public ResponseEntity<String> checkPayment(String imp_uid) {
+        String url = portOneUrl + "/payments/" + imp_uid;
+        System.out.println(isAccessTokenValid(expired_at));
         try {
-            if (accessToken == null && isAccessTokenValid(expired_at)) {
+            if (accessToken == null || !isAccessTokenValid(expired_at)) {
                 getAccessToken();
-                System.out.println("accessToken 발급");
+                System.out.println("Issue accessToken.");
             }else {
-                System.out.println("accessToken 이 유효합니다.");
+                System.out.println("accessToken is valid.");
             }
 
-            RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.set("Authorization", accessToken);
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(headers);
-            String url = portOneUrl + "/payments/" + imp_uid;
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+
+            return response;
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            return new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<String> cancelPayment(String imp_uid) {
+        String url = portOneUrl + "/payments/cancel";
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", accessToken);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            Map<String, String> body = new HashMap<>();
+            body.put("imp_uid", imp_uid);
+
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
 
             return response;
 
@@ -100,7 +127,8 @@ public class PaymentService {
     public boolean isAccessTokenValid(long expired_at) {
         long now = System.currentTimeMillis() / 1000L;
         long isExpired = expired_at - now;
-        return isExpired < 0;
+        System.out.println("isExpired : " + isExpired);
+        return expired_at > now;
     }
 
 }
